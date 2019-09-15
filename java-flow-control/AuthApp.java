dcd557af881f7696e84143888920d2fad89ddd77
egoing <egoing@gmail.com>
2019-09-15 15:30:36 +0900

    4.2. 조건문의 응용

---
+++ java-flow-control/AuthApp.java
@@ -0,0 +1,20 @@
+
+public class AuthApp {
+
+	public static void main(String[] args) {
+		
+		String id = "egoing";
+		String inputId = args[0];
+		
+		System.out.println("Hi.");
+		
+		//if(inputId == id) {
+		if(inputId.equals(id)) {
+			System.out.println("Master!");
+		} else {
+			System.out.println("Who are you?");
+		}
+
+	}
+
+}
